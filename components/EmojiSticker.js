8c05e2df951a95740164bab40e83d9fc1e6387a4
Jefry1217 <joshua.hallam127@gmail.com>
2023-06-26 04:40:15 +1000

    yeah

---
+++ components/EmojiSticker.js
@@ -0,0 +1,75 @@
+import { View, Image } from 'react-native';
+import { PanGestureHandler, TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+const AnimatedView = Animated.createAnimatedComponent(View);
+
+export default function EmojiSticker({ imageSize, stickerSource }) {
+
+  const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = withSpring(imageSize * 2);
+      }
+      else {
+        scaleImage.value = withSpring(imageSize);
+      }
+    },
+  });
+
+  const onDrag = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.translateX + event.translationX;
+      translateY.value = context.translateY + event.translationY;
+    },
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: scaleImage.value,
+      height: scaleImage.value,
+    };
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={onDrag} >
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
+        <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+          <AnimatedImage
+            source={stickerSource}
+            resizeMode="contain"
+            style={imageStyle}
+          />
+        </TapGestureHandler>
+      </AnimatedView>
+    </PanGestureHandler>
+  );
+}
